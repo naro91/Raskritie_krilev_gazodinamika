16065ac40ce69f2f7aeb9dc326f7fec71b78c3bc
naro19 <naro91@mail.ru>
2015-03-21 17:48:28 +0300

    Добавил коректную функцию для резьбы

--- src/main/java/function/interfaceFunction.java
+++ src/main/java/function/interfaceFunction.java
@@ -7,6 +7,5 @@ import java.util.HashMap;
  * Created by Abovyan Narek on 14.02.15.
  */
 public interface interfaceFunction {
-    abstract public String getName ();
     abstract public double calculate ( double x , HashMap<String, Double> values );
 }
